5a022faf711f9f50575e6d7021ae9b95ac896b11
Juan Lujan <jlujan@dnacenter.com>
2023-03-21 10:37:31 -0400

    added sign in

--- crwn-clothing/src/routes/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/routes/utils/firebase/firebase.utils.js
@@ -32,3 +32,32 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()) {
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try{
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt
+        })
+      }catch(error) {
+        console.log('error creting the user', error.message);
+      }
+    }
+
+    return userDocRef;
+  }
