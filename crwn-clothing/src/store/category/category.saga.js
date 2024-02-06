c67b481a0ed3a75a8eefbc3a786b955c4c10db2e
unknown <jlujan6@gatech.edu>
2024-02-06 09:15:27 -0500

    fixed issue

--- crwn-clothing/src/store/category/category.saga.js
+++ crwn-clothing/src/store/category/category.saga.js
@@ -1,6 +1,6 @@
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import {all, call, takeLatest, put} from 'redux-saga/effects';
-import {fetchCategoriesSuccess, fetchCategoriesFailure} from './categories.action';
+import {fetchCategoriesSuccess, fetchCategoriesFailed} from './categories.action';
 import { CATEGORIES_ACTION_TYPES } from "./categories.types";
 
 export function* fetchCategoriesAsync()  {
@@ -8,7 +8,7 @@ export function* fetchCategoriesAsync()  {
         const categoriesArray = yield call(getCategoriesAndDocuments, 'categories');
         yield put(fetchCategoriesSuccess(categoriesArray));
     } catch (error) {
-        yield put(fetchCategoriesFailure(error));
+        yield put(fetchCategoriesFailed(error));
     }
 }
 
